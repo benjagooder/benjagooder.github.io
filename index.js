2cdd196e6627c08409888ec553def7c39c10983d
Benja Gooder <benjamingooder@gmail.com>
2019-05-27 12:00:08 -0400

    Home statuses and blog SE principles list

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var listening = 'Midnight Organ Fight (album) - Frightened Rabbit';
+var reading = 'Deep Work - Cal Newport';
+var watching = 'Reservoir Dogs (1992)';
+var haiku1 = 'King simplicity';
+var haiku2 = 'Complexity is evil';
+var haiku3 = 'Where is this not true?';
 
+
+document.getElementById("musicThatIAmListeningTo").innerHTML = listening;
+document.getElementById("bookThatIAmReading").innerHTML = reading;
+document.getElementById("cinemaThatIAmWatching").innerHTML = watching;
+document.getElementById("newestHaiku1").innerHTML = haiku1;
+document.getElementById("newestHaiku2").innerHTML = haiku2;
+document.getElementById("newestHaiku3").innerHTML = haiku3;
